04dbe90585956eece1be48427a6726ed367a5b32
Shuvra Saha Rimjim <110848482+shuvra789@users.noreply.github.com>
2022-08-19 00:56:37 +0600

    Add files via upload

---
+++ lab5.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ void shape(void);
+
+ int main(void)
+ {
+     shape();
+     return 0;
+ }
+
+  void shape(void)
+  {
+      printf("      ()  \n");
+      printf("     ()() \n");
+      printf("    ()()()\n");
+      printf("     ()() \n");
+      printf("      ()  \n");
+
+  }
